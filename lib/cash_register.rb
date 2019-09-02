b6a956be35d86a587fa6add17d623f8ad58a169e
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-02 02:53:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,13 +2,24 @@ class CashRegister
   
   attr_accessor :total, :discount
   
+  @@all = []
+  
   def initialize(discount=nil)
     @total = 0
     @discount = discount
+    @@all << self 
   end
   
   def add_item(title, price, quantity=nil)
     @total += 1 
+    
+  end 
+  
+  def items 
+    @@all 
+  end 
+  
+  def void_last_transaction
     
   end 
 end
